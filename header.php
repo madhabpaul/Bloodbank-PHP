def7489655cfc3f48c53c3a559d8c662da71c5e6
Nirupam-paul <58510626+Nirupam-paul@users.noreply.github.com>
2021-02-05 01:18:09 +0530

    link fixed

--- header.php
+++ header.php
@@ -99,89 +99,15 @@
     					<div class="desktoplogo"><a href="#hero-1"><img src="images/images-logo-grey.png" width="180" height="40" alt="header-logo"></a></div>
 
     					<!-- MAIN MENU -->
-      					<nav class="wsmenu clearfix"><ul class="wsmenu-list"><!-- DROPDOWN MENU --><li aria-haspopup="true"><a href="#">Home <span class="wsarrow"></span></a>
-					           		<ul class="sub-menu"><li aria-haspopup="true"><a href="demo-2.html">Home Concept #2</a></li>
-					              		<li aria-haspopup="true"><a href="demo-3.html">Home Concept #3</a></li>
-					              		<li aria-haspopup="true"><a href="demo-4.html">Home Concept #4</a></li>
-					              		<li aria-haspopup="true"><a href="demo-5.html">Home Concept #5</a></li>
-					              		<li aria-haspopup="true"><a href="demo-6.html">Home Concept #6</a></li>
-					              		<li aria-haspopup="true"><a href="demo-7.html">Home Concept #7</a></li>
-					              		<li aria-haspopup="true"><a href="demo-8.html">Home Concept #8</a></li>
-					              		<li aria-haspopup="true"><a href="demo-9.html">Home Concept #9</a></li>
-					              		<li aria-haspopup="true"><a href="demo-10.html">Home Concept #10</a></li>
-					              		<li aria-haspopup="true"><a href="demo-11.html">Home Concept #11</a></li>
-					              		<li aria-haspopup="true"><a href="demo-12.html">Home Concept #12</a></li>
-					           		</ul></li>	<!-- END DROPDOWN MENU -->
-
-
-					          	<!-- PAGES -->
-					          	<li aria-haspopup="true"><a href="#">Pages <span class="wsarrow"></span></a>
-            						<div class="wsmegamenu clearfix">
-             							<div class="container">
-               								<div class="row">
-
-               									<!-- MEGAMENU LINKS -->
-               									<ul class="col-lg-3 col-md-12 col-xs-12 link-list"><li class="title">Standard Pages:</li>
-								                    <li><a href="about-us.html">About Us Page</a></li>
-								                    <li><a href="who-we-are.html">Who We Are</a></li>
-								                    <li><a href="all-services.html">Our Services</a></li>
-								                    <li><a href="service-1.html">Service Single #1</a></li>
-								                    <li><a href="service-2.html">Service Single #2</a></li>				                             
-								                </ul><!-- MEGAMENU LINKS --><ul class="col-lg-3 col-md-12 col-xs-12 link-list"><li class="title">Medical Pages:</li>
-								                    <li><a href="all-departments.html">Our Departments</a></li>								                    
-								                    <li><a href="department-single.html">Department Single</a></li>
-								                    <li><a href="all-doctors.html">Meet the Doctors</a></li>
-								                    <li><a href="doctor-1.html">Doctor's Profile #1</a></li>
-								                    <li><a href="doctor-2.html">Doctor's Profile #2</a></li>
-								                    <li><a href="timetable.html">Doctors Timetable</a></li>									                    
-								                </ul><!-- MEGAMENU LINKS --><ul class="col-lg-3 col-md-12 col-xs-12 link-list"><li class="title">Special Pages:</li>
-								                    <li><a href="pricing-1.html">Pricing Packages #1</a></li>
-								                    <li><a href="pricing-2.html">Pricing Packages #2</a></li>								                    
-								                    <li><a href="appointment.html">Make an Appointment</a></li>
-								                    <li><a href="faqs.html">FAQs Page</a></li>
-								                    <li><a href="terms.html">Terms of Use</a></li> 
-								                </ul><!-- MEGAMENU LINKS --><ul class="col-lg-3 col-md-12 col-xs-12 link-list"><li class="title">Auxiliary Pages:</li>
-								                    <li><a href="gallery.html">Our Gallery</a></li>
-								                    <li><a href="blog-listing.html">Blog Listing Page</a></li>
-								                    <li><a href="single-post.html">Single Blog Post</a></li>								                   
-								                    <li><a href="contacts-1.html">Contact Us #1</a></li>
-								                    <li><a href="contacts-2.html">Contact Us #2</a></li> 						
-								                </ul></div>  <!-- End row -->	
-							            </div>  <!-- End container -->	
-							        </div>  <!-- End wsmegamenu -->	
-							    </li>	<!-- END PAGES -->
-
-
-					          	<!-- HALF MENU -->
-					          	<li aria-haspopup="true"><a href="#">Half Menu <span class="wsarrow"></span></a>
-					            	<div class="wsmegamenu clearfix halfmenu">
-					              		<div class="container-fluid">
-					                		<div class="row">
-
-					                			<!-- Links -->
-							                	<ul class="col-lg-6 col-md-12 col-xs-12 link-list"><li class="title">For Patients:</li>
-								                    <li><a href="#">Meet The Doctors</a></li>							                    
-								                    <li><a href="#">Patient Info Sheets</a></li>
-								                    <li><a href="#">Online Patients Portal</a></li>
-								                    <li><a href="#">Patients Testimonials</a></li>
-								                    <li><a href="#">Blog &amp; Latest News</a></li>								                    
-							                  	</ul><!-- Links --><ul class="col-lg-6 col-md-12 col-xs-12 link-list"><li class="title">Quick Links:</li>
-								                    <li><a href="#">Terms &amp; Privacy Policy</a></li>
-								                    <li><a href="#">Donor Privacy Policy</a></li>
-								                    <li><a href="#">Workers Compensation</a></li>
-								                    <li><a href="#">Insurance Information</a></li>
-								                    <li><a href="#">After Hours Care</a></li>
-								                </ul></div>
-					              		</div>
-					            	</div>
-					          	</li>	<!-- END HALF MENU -->
-
-
-							    <!-- SIMPLE NAVIGATION LINK -->
-							    <li class="nl-simple" aria-haspopup="true"><a href="#">Simple Link</a></li>
+      					<nav class="wsmenu clearfix"><ul class="wsmenu-list">
+                  <li class="nl-simple" aria-haspopup="true"><a href="index.php">Home</a></li>
+                  <li class="nl-simple" aria-haspopup="true"><a href="camps.php">Camps</a></li>
+                  <li class="nl-simple" aria-haspopup="true"><a href="contact.php">Contact</a></li>
+                  <li class="nl-simple" aria-haspopup="true"><a href="">About</a></li>
+                  <li class="nl-simple" aria-haspopup="true"><a href="profile.php">Profile</a></li>
 
 							    <!-- NAVIGATION MENU BUTTON -->
-							    <li class="nl-simple header-btn" aria-haspopup="true"><a href="appointment.html">Make an Appointment</a></li>
+							    <li class="nl-simple header-btn" aria-haspopup="true"><a href="Request.php">Make a Request</a></li>
 
 
         					</ul></nav><!-- END MAIN MENU --></div>
